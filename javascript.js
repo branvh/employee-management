fb904212cd35adbd1515181599a1b6373601be89
tyleryonke <tyleryonke@gmail.com>
2016-11-12 10:46:28 -0600

    added firebase login

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 
 //firebase database key and creation
+var config = {
+    apiKey: "AIzaSyCfj9gCrj_ataltYC6fK18TJRU6-xCflh0",
+    authDomain: "employeefirebase.firebaseapp.com",
+    databaseURL: "https://employeefirebase.firebaseio.com",
+    storageBucket: "employeefirebase.appspot.com"
+};
+
+firebase.initializeApp(config);
 
 //on page load, do an ajax call to update the domm as the callback
 
